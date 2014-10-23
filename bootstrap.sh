47da30faf2da45ca238232be59ec1a69a81f019f
Richard <willis.rh@gmail.com>
2014-10-23 15:55:28 +0100

    Install git and run build script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,20 @@
 #!/usr/bin/env bash
 # boostrap.sh - used by vagrant to provision a dev environment
 
-cat > /etc/apt/sources.list << EOL
-  deb mirror://mirrors.ubuntu.com/mirrors.txt trusty main restricted universe multiverse
-  deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-updates main restricted universe multiverse
-  deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-backports main restricted universe multiverse
-  deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-security main restricted universe multiverse
-EOL
+# Adjust apt mirrors for faster package downloads
+sed -i "s/archive.ubuntu.com/gb.archive.ubuntu.com/g" /etc/apt/sources.list
 
 # Install OS dependencies
 apt-get update && apt-get upgrade -y
 apt-get install -y python-software-properties
 
 add-apt-repository ppa:chris-lea/node.js && apt-get update
-apt-get install -y supervisor build-essential nodejs ruby-dev
+apt-get install -y git supervisor build-essential nodejs ruby-dev
 
 gem install --no-ri --no-rdoc bundler
 
 # Install app dependencies
-cd /var/www && npm install && bundle install
+cd /var/www && bundle install && npm install && npm run build
 
 # Start services
 supervisord -c /var/www/supervisord.conf
